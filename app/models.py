5ed7b36e992abeb051c10e86ab4ddedb3a3fa0e6
Michael Kiptoo <michaelkiptoo2000@gmail.com>
2024-02-03 19:37:53 +0300

    Add serializer rules

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ db = SQLAlchemy()
 class Restaurant(db.Model,  SerializerMixin):
     __tablename__ = 'restaurants'
 
+    serialize_rules= ('-restaurants_pizzas.restaurant',)
+
     id = db.Column(db.Integer, primary_key=True)
     name=  db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(256))
@@ -18,6 +20,7 @@ class Restaurant(db.Model,  SerializerMixin):
 
 class Pizza(db.Model,  SerializerMixin):
     __tablename__= 'pizzas'
+    serialize_rules= ('-restaurants_pizzas.pizza',)
     
     id=  db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -32,6 +35,7 @@ class Pizza(db.Model,  SerializerMixin):
     
 class Restaurant_pizza(db.Model, SerializerMixin):
     __tablename__='restaurant_pizzas'
+    serialize_rules= ('-pizza.restaurant_pizzas', '-restaurant.restaurant_pizzas')
     
     id=  db.Column(db.Integer, primary_key=True)
     pizza_id= db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
@@ -45,6 +49,6 @@ class Restaurant_pizza(db.Model, SerializerMixin):
     
     @validates
     def validate_price(self, key, price):
-        if  price is None and price <1 and price >30:
+        if  price is None and price==range(1,30):
             raise ValueError ("Price must be between 1 and 30")
         return  price  
